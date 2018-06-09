a5ecc683548c6c92cb47ad738d4251bce1c3ccd4
Kaien <manmitsu@hotmail.fr>
2018-06-09 13:06:08 +0200

    Première version du portfolio

---
+++ README.md
@@ -0,0 +1,1 @@
+# kirinraikage.github.io
---
+++ script/index.js
@@ -0,0 +1,19 @@
+var images = document.querySelectorAll(".containerIcones i");
+var boutonLearn = document.querySelector("#knowMore");
+
+function survolImages(listeImages) {
+    for (var i = 0; i < images.length; i++) {
+        images[i].addEventListener("mouseover", function () {
+            this.classList.toggle("colored");
+        });
+        images[i].addEventListener("mouseout", function () {
+            this.classList.toggle("colored");
+        });
+
+
+    }
+}
+boutonLearn.addEventListener("click", function () {
+    window.location.href = "#containerCompetences";
+});
+survolImages(images);
