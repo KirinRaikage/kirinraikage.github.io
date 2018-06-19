9f236aea7b19628b0124eef6e7beb276de5ed29a
Kaien <manmitsu@hotmail.fr>
2018-06-19 15:06:47 +0200

    Suppression fullPage

--- script/index.js
+++ script/index.js
@@ -1,21 +1,13 @@
-// Chargement de fullpage.js
-$(document).ready(function () {
-    $('#fullpage').fullpage();
-});
-
 var images = document.querySelectorAll(".containerIcones i");
 var boutonLearn = document.querySelector("#knowMore");
 function survolImages(listeImages) {
     for (var i = 0; i < images.length; i++) {
         images[i].addEventListener("mouseover", function () {
-            
             this.classList.toggle("colored");
         });
         images[i].addEventListener("mouseout", function () {
             this.classList.toggle("colored");
         });
-
-
     }
 }
 boutonLearn.addEventListener("click", function () {
