d3d564a6e3e82d4ebfae8ec896cff5f600905a22
Kaien <manmitsu@hotmail.fr>
2018-06-19 11:51:12 +0200

    Mise à jour du Devicon + mise à jour contenu

--- script/index.js
+++ script/index.js
@@ -1,9 +1,14 @@
+// Chargement de fullpage.js
+$(document).ready(function () {
+    $('#fullpage').fullpage();
+});
+
 var images = document.querySelectorAll(".containerIcones i");
 var boutonLearn = document.querySelector("#knowMore");
-
 function survolImages(listeImages) {
     for (var i = 0; i < images.length; i++) {
         images[i].addEventListener("mouseover", function () {
+            
             this.classList.toggle("colored");
         });
         images[i].addEventListener("mouseout", function () {
@@ -14,6 +19,6 @@ function survolImages(listeImages) {
     }
 }
 boutonLearn.addEventListener("click", function () {
-    window.location.href = "#containerCompetences";
+    window.location.href = "#containerCompetences"; //Redirige vers le div correspondant
 });
 survolImages(images);
